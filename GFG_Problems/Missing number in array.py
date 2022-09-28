676652843a696ddb1f1f3451c8e1cea8e6e58316
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-09-28 20:48:49 +0530

    Create Missing number in array.py

---
+++ GFG_Problems/Missing number in array.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+
+
+
+class Solution:
+    def MissingNumber(self,array,n):
+        # code here
+        fs = (n*(n+1))//2
+        return fs - sum(array)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=Solution().MissingNumber(a,n)
+    print(s)
+# } Driver Code Ends
