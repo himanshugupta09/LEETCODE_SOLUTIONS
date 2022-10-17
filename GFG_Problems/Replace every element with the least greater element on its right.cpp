550ac5d88a800a3a1b1b172dd8ade2a7ba0e6019
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-10-17 21:07:10 +0530

    Create Replace every element with the least greater element on its right.cpp

---
+++ GFG_Problems/Replace every element with the least greater element on its right.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+    public:
+    vector<int> findLeastGreater(vector<int>& arr, int n) {
+        
+        vector<int>ans;
+        set<int>s;
+        for(int i=n-1;i>=0;i--)
+        {
+            auto up = s.upper_bound(arr[i]);
+            if(up == s.end())
+            {
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(*up);
+                
+            }
+            s.insert(arr[i]);
+            
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    vector<int>arr(n);
+	    for(int i = 0 ; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findLeastGreater(arr, n);
+	    for(auto it : ans){
+	        cout << it << " ";
+	    }
+	    cout <<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
