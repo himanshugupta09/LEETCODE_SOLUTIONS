090894eac06da02ab620f8008defd762addddc92
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-12-10 11:37:59 +0530

    Create binary-search.py

---
+++ Searching_Algorithms/binary-search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while right >= left:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target :
+                right = mid - 1
+            else:
+                left = mid + 1
+        return(-1)   
