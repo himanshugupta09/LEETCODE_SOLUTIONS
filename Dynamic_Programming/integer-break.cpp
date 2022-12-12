5bb014695fe41db2cb7ae4dcab105305a7cdfc82
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2022-12-12 18:59:09 +0530

    Create integer-break.cpp

---
+++ Dynamic_Programming/integer-break.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        int ans = 1;
+
+        if(n == 1 || n == 2)
+        {
+            return 1;
+        }
+        if(n == 3)
+        {
+            return 2;
+        }
+        else
+        {
+            vector<int>dp(n+1,-1);
+            dp[1] = 1;
+            
+            for(int i=2;i<=n;i++)
+            {
+                for(int j=1;j<i;j++)
+                {
+                    dp[i] = max(dp[i], (max(j,dp[j])) * (max(i - j, dp[i - j])));
+                }
+            }
+
+
+            return dp[n];
+            
+
+
+        }
+
+    }
+};
