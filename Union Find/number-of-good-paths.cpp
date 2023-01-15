0d0115d1ab3067773fa4dd83aad58c4e5a18534c
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-01-15 21:10:30 +0530

    Create number-of-good-paths.cpp

---
+++ Union Find/number-of-good-paths.cpp
@@ -0,0 +1,93 @@
+class UnionFind
+{
+    private:
+        vector<int>parent,rank;
+    
+    public:
+
+        UnionFind(int size)
+        {
+            parent.resize(size);
+            rank.resize(size,0);
+
+            for(int i=0;i<size;i++)
+            {
+                parent[i] = i;
+            }
+
+        }
+        int find(int x)
+        {
+            if(parent[x] != x)
+            {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+        
+        void union_set(int x, int y) {
+        int xset = find(x), yset = find(y);
+        if (xset == yset) {
+            return;
+        } else if (rank[xset] < rank[yset]) {
+            parent[xset] = yset;
+        } else if (rank[xset] > rank[yset]) {
+            parent[yset] = xset;
+        } else {
+            parent[yset] = xset;
+            rank[xset]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+        
+        int n = vals.size();
+        vector<vector<int>>adj(n);
+
+        for(auto& edge:edges)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+
+        map<int,vector<int>>valuesToNodes;
+
+        for(int i=0;i<n;i++)
+        {
+            valuesToNodes[vals[i]].push_back(i);
+        }
+
+
+        UnionFind dsu(n);
+        int goodPaths = 0;
+
+        // Iterate over all the nodes with the same value in sorted order, starting from the lowest
+        // value.
+        for (auto& [value, nodes] : valuesToNodes) {
+            // For every node in nodes, combine the sets of the node and its neighbors into one set.
+            for (int node : nodes) {
+                for (int neighbor : adj[node]) {
+                    // Only choose neighbors with a smaller value, as there is no point in
+                    // traversing to other neighbors.
+                    if (vals[node] >= vals[neighbor]) {
+                        dsu.union_set(node, neighbor);
+                    }
+                }
+            }
+            
+            unordered_map<int, int> group;
+            
+            for (int u : nodes) {
+                group[dsu.find(u)]++;
+            }
+            for (auto& [_, size] : group) {
+                goodPaths += (size * (size + 1) / 2);
+            }
+        }
+        return goodPaths;
+    }
+};
