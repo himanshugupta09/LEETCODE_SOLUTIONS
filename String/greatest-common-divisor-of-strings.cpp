9a2c3b35407888738320faf63d655596e8995c00
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-02-01 18:50:33 +0530

    Create greatest-common-divisor-of-strings.cpp

---
+++ String/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        
+        if(str1 + str2 != str2 + str1)
+        {
+            return "";
+        }
+
+        int Lent = gcd(str1.size(),str2.size());
+
+        return str1.substr(0,Lent);
+
+    }
+};
