9ab645e81e7f9550b7037aa18ddd696d0fe3348e
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-02-18 14:58:10 +0530

    Create invert-binary-tree.cpp

---
+++ TREES/invert-binary-tree.cpp
@@ -0,0 +1,77 @@
+/*
+
+================================== Recursive Solution =================================================================================================
+
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* invertTree(TreeNode* root) {
+       if(root == NULL)
+       {
+           return root;
+       }
+       swap(root->left,root->right);
+       invertTree(root->left);
+       invertTree(root->right);
+
+       return root;
+
+    }
+};
+
+/*
+
+
+============================================================ Iterative Solution ==============================================================================
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)return NULL;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* curr=q.front();
+            q.pop();
+
+            TreeNode* temp =curr->left;
+            curr->left=curr->right;
+            curr->right=temp;
+
+
+            if(curr->left)
+                q.push(curr->left);
+            if(curr->right)
+                q.push(curr->right);
+
+        }
+        return root;
+    }
+};
