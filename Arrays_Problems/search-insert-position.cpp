dc77063d4a4820d2e4d79a60a6e1f9d78468af9b
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-02-20 14:01:17 +0530

    Create search-insert-position.cpp

---
+++ Arrays_Problems/search-insert-position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size();
+
+        while(l < r)
+        {
+            int mid = (l+r)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+        return l;
+    }
+};
