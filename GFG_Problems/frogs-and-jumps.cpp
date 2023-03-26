b2fcc7be6a3553f74d8509a3fadd0928a396041a
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-03-26 15:22:42 +0530

    Create frogs-and-jumps.cpp

---
+++ GFG_Problems/frogs-and-jumps.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        // Code here
+        vector<bool>vis(leaves+1,false);
+        
+        for(int i=0;i<N;i++)
+        {
+            int n = frogs[i];
+            if(n <= leaves && !vis[n])
+            {
+                for(int k=n;k<=leaves;k+=n)
+                {
+                    vis[k] = true;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=1;i<=leaves;i++)
+        {
+            if(!vis[i])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, leaves;
+        cin >> N >> leaves;
+        int frogs[N];
+        for (int i = 0; i < N; i++) {
+            cin >> frogs[i];
+        }
+
+        Solution ob;
+        cout << ob.unvisitedLeaves(N, leaves, frogs) << endl;
+    }
+}
+// } Driver Code Ends
