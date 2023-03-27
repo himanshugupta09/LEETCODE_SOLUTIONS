db62c5932d606fb81e09974a22453fe073bd32b4
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-03-27 20:33:45 +0530

    Create leaders-in-array.cpp

---
+++ GFG_Problems/leaders-in-array.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        vector<int>res;
+        res.push_back(a[n-1]);
+        int max_red = a[n-1];
+        for(int i=2;i<=n;i++)
+        {
+            if(a[n-i] >= max_red)
+            {
+                max_red = a[n-i];
+                res.push_back(max_red);
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+
+// } Driver Code Ends
