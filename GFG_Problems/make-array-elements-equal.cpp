8e0f544065e31bcea0cc4cd63d4424e5379695a1
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-04-01 18:17:35 +0530

    Create make-array-elements-equal.cpp

---
+++ GFG_Problems/make-array-elements-equal.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minOperations(int N) {
+        // Code here
+        if(N == 1)
+        {
+            return 1;
+        }
+        long long int cn = N;
+        long long int hi = N/2;
+        
+        long long int res = hi*cn-hi*hi;
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
