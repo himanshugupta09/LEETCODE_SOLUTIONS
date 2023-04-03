f8ce18c1455466aaecad6a8cfe1e8827d3eaf209
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-04-03 21:00:38 +0530

    Create boats-to-save-people.cpp

---
+++ Arrays_Problems/boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        sort(people.begin(),people.end());
+
+        int i=0,j=people.size()-1;
+        int res = 0;
+
+        while(i <= j)
+        {
+            res++;
+            if(people[i]+people[j] <= limit)
+            {
+                i++;
+            }
+            j--;
+        }
+        return res;
+    }
+};
