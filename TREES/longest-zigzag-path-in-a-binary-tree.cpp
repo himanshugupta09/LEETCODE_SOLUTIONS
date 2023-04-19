8150aa107932c0a495c0ec19461b8756608aea97
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-04-19 22:35:35 +0530

    Create longest-zigzag-path-in-a-binary-tree.cpp

---
+++ TREES/longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int path = 0;
+    void dfs(TreeNode* root,bool goLeft,int steps)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        path = max(path,steps);
+        if(goLeft)
+        {
+            dfs(root->left,false,steps+1);
+            dfs(root->right,true,1);
+        }
+        else
+        {
+            dfs(root->left,false,1);
+            dfs(root->right,true,steps+1);
+
+        }
+    }
+
+    int longestZigZag(TreeNode* root) {
+        
+        dfs(root,false,0);
+        dfs(root,true,0);
+
+
+        return path;
+    }
+};
