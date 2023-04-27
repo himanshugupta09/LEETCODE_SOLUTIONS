f8a283ed46e0edb0bb299e1c9415242e577b14c2
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-04-27 19:02:04 +0530

    Create bulb-switcher.cpp

---
+++ Maths/bulb-switcher.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        if(n >= 1)
+        {  return sqrt(n);
+        }
+        else
+        {
+            return 0;
+        }
+        
+    }
+};
