d3a87df5484c343b03b6b4ef1cdbd64d271d0244
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-05-11 12:03:50 +0530

    Create function-compostion.js

---
+++ 30_Days_JavaScript/function-compostion.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        if(functions.length == 0) return x;
+        let input = x;
+
+        for(let i = functions.length - 1;i >= 0;i--)
+        {
+            const curF = functions[i];
+
+            input = curF(input);
+        }
+        return input;
+        
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
