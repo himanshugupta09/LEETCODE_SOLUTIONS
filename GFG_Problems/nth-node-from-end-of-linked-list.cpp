3bd37896d88903ace3c9c3cdd28d75e13ec17a2e
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-05-28 20:30:05 +0530

    Create nth-node-from-end-of-linked-list.cpp

---
+++ GFG_Problems/nth-node-from-end-of-linked-list.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+
+
+// } Driver Code Ends
+//User function Template for C
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(struct Node *head, int n)
+{
+    int ln = 0;
+    struct Node* pr = NULL;
+    struct Node* cur = head;
+    
+    while(cur)
+    {
+        struct Node* nx = cur->next;
+        cur->next = pr;
+        pr = cur;
+        cur = nx;
+        ln++;
+    }
+    if(ln < n)
+    {
+        return -1;
+        
+    }
+    int i = 1;
+    while(i != n)
+    {
+        pr = pr->next;
+        i++;
+    }
+    return pr->data;
+}
+
+//{ Driver Code Starts.
+
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      int n,k;
+      scanf("%d",&n);
+      scanf("%d",&k);
+      insert(n);
+      int res = getNthFromLast(start,k);
+      printf("%d\n",res);
+    }
+    return 0;
+
+}
+
+
+ void insert(int n)
+ {   int value,i;
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+
+
+
+
+
+// } Driver Code Ends
