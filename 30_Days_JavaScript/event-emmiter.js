7a5b9b3496d1ba1df2a76cd3c7add2137e9294dc
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-05-31 20:09:15 +0530

    Create event-emmiter.js

---
+++ 30_Days_JavaScript/event-emmiter.js
@@ -0,0 +1,38 @@
+class EventEmitter {
+    constructor(){
+        this.events = {};
+    }
+    
+  subscribe(event, cb) {
+      this.events[event] = this.events[event] ?? [];
+      this.events[event].push(cb);
+      
+    return {
+        unsubscribe: () => {
+            this.events[event] = this.events[event].filter(f => f !== cb);
+            if(this.events[event].legth === 0)
+            {
+                delete this.events[event]
+            }
+        }
+    };
+  }
+
+  emit(event, args = []) {
+      if(!(event in this.events)) return [];
+      return this.events[event].map(f => f(...args));
+
+  }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
