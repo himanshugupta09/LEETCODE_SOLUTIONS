d1d5aef8d4f7f4768bf6b5541fa4c72186b14232
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-06-18 20:58:02 +0530

    Create ticket-counter.cpp

---
+++ GFG_Problems/ticket-counter.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        // code here
+        
+        int se = ceil((float)N/K);
+        
+        if(se%2 == 1)
+        {
+            return N-se/2*K;
+        }
+        else
+        {
+            return se/2*K+1;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
