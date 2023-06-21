8c2a2c795c41ac7a348f9367b240d267f32d3583
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-06-21 22:54:43 +0530

    Create reverse-coding.cpp

---
+++ GFG_Problems/reverse-coding.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        
+        int m = 1e9+7;
+       
+        int ans = ((((long)n*(long)(n+1))%m)*500000004)%m;
+       
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
