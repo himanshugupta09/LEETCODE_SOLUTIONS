0f4e05578efbdc5954e220222bbc93c20c9f2215
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-07-02 18:59:16 +0530

    Create copy-set-bits-in-range.cpp

---
+++ BIT MANIPULATION/copy-set-bits-in-range.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        // code here
+        
+        int msk = (1 << (r-l+1));
+        msk--;
+        msk = (msk << (l-1));
+        msk = msk&y;
+        x = x|msk;
+        return x;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, l, r;
+        cin>>x>>y>>l>>r;
+        
+        Solution ob;
+        cout<<ob.setSetBit(x, y, l, r)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
