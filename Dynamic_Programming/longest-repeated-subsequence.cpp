dce7133d4d27d1db32a0e7f26c9b9478f4e510b9
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-07-18 21:15:26 +0530

    Create longest-repeated-subsequence.cpp

---
+++ Dynamic_Programming/longest-repeated-subsequence.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    int n = str.size();
+		    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+		    
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=1;j<=n;j++)
+		        {
+		            if((i != j) && (str[i-1] == str[j-1]))
+		            {
+		                dp[i][j] = dp[i-1][j-1]+1;
+		            }
+		            else
+		            {
+		                dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+		            }
+		        }
+		    }
+		    return dp[n][n];
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
