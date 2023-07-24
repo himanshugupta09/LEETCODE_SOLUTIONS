7c368ad130dc4640faa2081f2c68208dee27e324
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-07-24 20:15:32 +0530

    Create rotate-image.cpp

---
+++ Arrays_Problems/rotate-image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        reverse(matrix.begin(),matrix.end());
+
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
