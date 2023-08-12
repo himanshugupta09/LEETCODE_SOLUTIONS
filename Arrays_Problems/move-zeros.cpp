b164b34e3ef3a5f23e8ab8e3d474da9cf3921ac0
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-08-12 21:40:40 +0530

    Create move-zeros.cpp

---
+++ Arrays_Problems/move-zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        for(int i=0,j=0;j<nums.size();j++)
+        {
+            if(nums[j] != 0)
+            {
+                swap(nums[i++],nums[j]);
+            }
+        }
+    }
+};
