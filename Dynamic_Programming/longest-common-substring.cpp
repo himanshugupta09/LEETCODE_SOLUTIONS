75181dc7e65b22f4b69379d460d0dc82ed7c6a75
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-08-30 09:53:41 +0530

    longest-common-substring.cpp

---
+++ Dynamic_Programming/longest-common-substring.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        // your code here
+        int res = 0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(S1[i-1] == S2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                res = max(res,dp[i][j]);
+                
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
