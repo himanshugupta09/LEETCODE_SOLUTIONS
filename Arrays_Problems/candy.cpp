77526a21b8e10d67d53d9cd36a310d43d58c9ab1
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-09-13 16:40:02 +0530

    candy.cpp

---
+++ Arrays_Problems/candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        
+        int n = ratings.size();
+        vector<int>can(n,1);
+
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i] > ratings[i-1])
+            {
+                can[i]  = can[i-1]+1;
+            }
+        }
+        int sm = can[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i] > ratings[i+1])
+            {
+                can[i] = max(can[i],(can[i+1]+1));
+            }
+            sm += can[i];
+        }
+        return sm;
+
+
+    }
+};
