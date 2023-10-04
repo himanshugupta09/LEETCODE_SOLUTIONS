e9b02a74b1f8c51ba8555c705157c247003987c9
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-10-04 18:43:29 +0530

    design-hash-map.cpp

---
+++ Basic-DSA-Quest/design-hash-map.cpp
@@ -0,0 +1,27 @@
+class MyHashMap {
+public:
+    int mp[1000001];
+    MyHashMap() {
+        fill(mp,mp+ 1000000, -1);
+    }
+    
+    void put(int key, int value) {
+        mp[key] = value;
+    }
+    
+    int get(int key) {
+        return mp[key];
+    }
+    
+    void remove(int key) {
+        mp[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
