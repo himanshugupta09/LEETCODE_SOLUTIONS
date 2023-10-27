bcb256577fbbe6c9dc9abc26b50cac2f03b92dc7
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-10-27 19:55:01 +0530

    longest-palindromic-substring.cpp

---
+++ Dynamic_Programming/longest-palindromic-substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        int n = s.length();
+        vector<vector<bool>> dp(n,vector<bool>(n, false));
+        int ans[2] = {0, 0};
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                ans[0] = i;
+                ans[1] = i + 1;
+            }
+        }
+
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+
+        int i = ans[0];
+        int j = ans[1];
+        return s.substr(i, j - i + 1);
+        
+        
+    }
+};
