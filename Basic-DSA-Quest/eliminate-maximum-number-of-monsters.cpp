dc01b6728e6a8d10f0d70355dab92288d3b75e28
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-11-07 19:31:07 +0530

    eliminate-maximum-number-of-monsters.cpp

---
+++ Basic-DSA-Quest/eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        
+        vector<float>time;
+        for(int i=0;i<dist.size();i++)
+        {
+            time.push_back((float) dist[i]/speed[i]);
+        }
+        sort(time.begin(),time.end());
+        
+        int res = 0;
+        for(int i=0;i<time.size();i++)
+        {
+            if(time[i] <= i)
+            {
+                break;
+            }
+            
+                res++;
+            
+        }
+        return res;
+    }
+};
