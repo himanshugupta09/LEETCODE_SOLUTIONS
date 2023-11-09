bed9307bd063975dda0adbdc9532cb15d96bc33a
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-11-09 23:58:43 +0530

    Create predict-the-column.cpp

---
+++ Basic-DSA-Quest/predict-the-column.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    /*Function to count zeros in each column
+    * N : Number of rows and columns in array
+    M is the matrix that is globally declared
+    */
+    int columnWithMaxZeros(vector<vector<int>>arr,int N){
+        
+        // Your code here
+        
+        // Your code here
+        int min=N, s=0, col=-1;
+        for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                s+=arr[j][i];
+            }
+            if (s<min){
+                col=i;
+                min=s;
+            }
+            s=0;
+        }
+        return col;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    vector<vector<int>>arr(N,vector<int>(N));
+	    // Taking elements input into the matrix
+	    for(int i = 0;i<N;i++){
+	        for(int j = 0;j<N;j++){
+	            int x;
+	            cin >> x; 
+	            arr[i][j]=x;
+	        }
+	    }
+	    Solution ob;
+	    cout << ob.columnWithMaxZeros(arr,N) << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
