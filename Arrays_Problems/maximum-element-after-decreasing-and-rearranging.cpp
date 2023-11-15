bf7e686b5b3701a6021e291bfeeab3d4181cedf0
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-11-15 15:25:35 +0530

    Create maximum-element-after-decreasing-and-rearranging.cpp

---
+++ Arrays_Problems/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        
+        if(arr.size() == 1)
+        {
+            return 1;
+        }
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        arr[0] = 1;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]-arr[i-1] > 1)
+            {
+                arr[i] = arr[i-1]+1;
+            }
+        }
+        return arr[n-1];
+        
+        
+    }
+};
