5f87883441edfb68cdc828164120b32b1495aa61
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2023-12-10 17:42:54 +0530

    <GFG POTD>

---
+++ GFG_Problems/subarray-with-sum-0.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        ##Your code here
+        #Return true or false
+        mp = {}
+        sm = 0
+        mp[0] = 1
+        for i in arr:
+            sm += i
+            if sm in mp:
+                mp[sm] += 1
+            else:
+                mp[sm] = 1
+            if mp[sm] > 1:
+                return True
+        return False
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
