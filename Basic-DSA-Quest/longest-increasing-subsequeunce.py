f6a3030f5e23894f15f87fa615ebe311ac937cc0
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-01-05 20:07:49 +0530

    Create longest-increasing-subsequeunce.py

---
+++ Basic-DSA-Quest/longest-increasing-subsequeunce.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+       
+        
+        dp = [1]*(n)
+        
+        for i in range(1,n):
+            for j in range(0,i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[j]+1,dp[i])
+        
+        return max(dp)
