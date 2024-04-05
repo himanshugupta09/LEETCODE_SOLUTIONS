e5e6a2eb57ac65a4c3835e287a731cac4e3acd47
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-04-05 21:02:17 +0530

    Create convert-to-strictly-increasing-array.cpp

---
+++ GFG_Problems/convert-to-strictly-increasing-array.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int LIS(vector<int>&nums)
+    {
+        int n = nums.size();
+        vector<int>dp(n,1);
+        int mx = -1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    dp[i] = max(dp[j]+1,dp[i]);
+                    mx = max(mx,dp[i]);
+                }
+            }
+            
+        }
+        return *max_element(dp.begin(),dp.end());
+        
+    }
+    int min_operations(vector<int>& nums) {
+       int n = nums.size(), maxi = 1;
+        vector<int> dp(n, 1);
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j] and (nums[i] - nums[j] >= (i - j))) {
+                    dp[i] = max(1 + dp[j], dp[i]);
+                    maxi = max(maxi, dp[i]);
+                }
+            }
+        }
+        
+        return n - maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+            cin >> nums[i];
+        Solution ob;
+        int ans = ob.min_operations(nums);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
