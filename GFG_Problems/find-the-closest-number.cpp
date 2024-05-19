b9bb2fef4f4e518ae9ea3bb183396dc25b5f9ec3
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-05-19 19:15:47 +0530

    Create find-the-closest-number.cpp

---
+++ GFG_Problems/find-the-closest-number.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int findClosest( int n, int k,int arr[]) 
+    { 
+        // Complete the function
+        int l = 0;
+        int r = n-1;
+        int res = arr[0];
+        int idx = -1;
+        while(l <= r)
+        {
+            int m = (l+r)/2;
+            if(abs(arr[m]-k)  == abs(res-k))
+            {
+                res = max(res,arr[m]);
+            }
+            else if(abs(k-arr[m]) < abs(k-res))
+            {
+               res = arr[m];
+            }
+            if(arr[m] == k)
+            {
+                return arr[m];
+            }
+            else if(arr[m] < k)
+            {
+               l = m+1;
+            }
+            else
+            {
+                r = m-1;
+            }
+        }
+        return res;
+        
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.findClosest(n,k,arr)<<endl;
+    }
+}
+
+// } Driver Code Ends
