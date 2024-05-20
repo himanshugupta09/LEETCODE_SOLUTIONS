8d086a3bd30525a936a789552bfcce931ed9f737
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-05-20 20:40:49 +0530

    Create sum-of-all-subset-xor-totals.cpp

---
+++ BIT MANIPULATION/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    
+    int subsetXORSum(vector<int>& nums) {
+        int res = 0;
+        for(int num:nums)
+        {
+            res |= num;
+        }
+        return res << (nums.size()-1);
+    }
+};
