1b72413389a966ac1bc2bb95e315bd5178721f62
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-05-25 10:45:18 +0530

    Create you-and-your-books.cpp

---
+++ GFG_Problems/you-and-your-books.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+         long long ans = 0; long long curr = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] <= k){
+                curr += arr[i];
+            }
+            else curr = 0;
+            ans = max(ans, curr);
+        }
+        return ans;
+    
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
