878229882fa7bf4b615eb7fb9e1ba492b4fc2289
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-06-02 20:10:41 +0530

    Create reverse-string.cpp

---
+++ Basic-DSA-Quest/reverse-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int n = s.size();
+        
+        int i = 0;
+        int j = n-1;
+        
+        while(i < j)
+        {
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+        
+        
+    }
+};
