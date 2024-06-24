25a34327feccb56888b7eefaa5a87c633a7d52ca
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-06-24 21:52:22 +0530

    Create summed-matrixx.cpp

---
+++ GFG_Problems/summed-matrixx.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        if (q < 2 || q > 2 * n) {
+            return 0;
+        }
+        long long start = max(1LL, q - n);
+        long long end = min(n, q - 1);
+        return end - start + 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
