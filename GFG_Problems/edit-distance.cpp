9b344e14decc767479a16006affb9aafdad3f581
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-08-02 19:53:19 +0530

    Create edit-distance.cpp

---
+++ GFG_Problems/edit-distance.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int editDistance(string str1, string str2) {
+        // Code here
+        int n = str1.size();
+        int m = str2.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0] = i;
+        }
+        for(int j=0;j<=m;j++)
+        {
+            dp[0][j] = j;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(str1[i-1] == str2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min({ dp[i - 1][j],    // Deletion
+                                     dp[i][j - 1],    // Insertion
+                                     dp[i - 1][j - 1] // Substitution
+                                   });
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
