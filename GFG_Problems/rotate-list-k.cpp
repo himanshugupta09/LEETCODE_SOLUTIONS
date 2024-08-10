c442af4a32d7d689e615376d22e95d13a9e3fa0b
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-08-10 20:27:30 +0530

    Create rotate-list-k.cpp

---
+++ GFG_Problems/rotate-list-k.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    // Function to rotate a linked list.
+    Node* rotate(Node* head, int k) {
+        // Your code here
+        Node* st = NULL,*end = head,*p = head;
+        int i = 1;
+        while(end->next != NULL)
+        {
+            if(k == i){
+                st = end;
+            }
+            end = end->next;
+            i++;
+        }
+        if(st == NULL)
+        {
+            return head;
+        }
+        head = st->next;
+        st->next = NULL;
+        end->next = p;
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.rotate(head, k);
+        printList(head);
+    }
+    return 1;
+}
+
+// } Driver Code Ends
