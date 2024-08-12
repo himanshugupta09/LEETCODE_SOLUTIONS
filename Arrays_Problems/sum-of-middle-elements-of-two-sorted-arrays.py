d2ffbf5d7553d4e50f210d0adc0916bd608f136f
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-08-12 17:01:22 +0530

    Create sum-of-middle-elements-of-two-sorted-arrays.py

---
+++ Arrays_Problems/sum-of-middle-elements-of-two-sorted-arrays.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def sum_of_middle_elements(self, arr1, arr2):
+        # code here
+        m1 = arr1[len(arr1)//2]
+        m2 = arr2[len(arr2)//2]
+        return m1+m2
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+
+input = sys.stdin.read
+
+
+def main():
+    input_lines = input().strip().split("\n")
+    t = int(input_lines[0])
+
+    index = 1
+    results = []
+    while t > 0:
+        arr = list(map(int, input_lines[index].strip().split()))
+        brr = list(map(int, input_lines[index + 1].strip().split()))
+        index += 2
+
+        solution = Solution()
+        res = solution.sum_of_middle_elements(arr, brr)
+        results.append(res)
+
+        t -= 1
+
+    for result in results:
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
