6515566e6b3ef57f3928b26c17e00569625b0a3b
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-10-02 20:29:42 +0530

    Create rotate-and-delete.cpp

---
+++ GFG_Problems/rotate-and-delete.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int rotateDelete(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        int lim = n/2;
+        
+        int i=3*(lim/2);
+        if(lim%2 == 1) i++;
+        return arr[n-i];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.rotateDelete(arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
