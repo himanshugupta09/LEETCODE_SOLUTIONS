2e908f843db9801e10bf20fdae2c87fa87e0775d
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-10-05 16:06:09 +0530

    Create not-a-subset-sum.cpp

---
+++ Arrays_Problems/not-a-subset-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long findSmallest(vector<int> &arr) {
+        // Your code goes here.
+        int n = arr.size();
+        long long res = 1;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] > res) break;
+            res += arr[i];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        auto ans = ob.findSmallest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
