f716faf8e8ad0fd028020b30914c2ee90742bf12
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-10-15 19:25:46 +0530

    Create subarray-range-with-given-sum.cpp

---
+++ GFG_Problems/subarray-range-with-given-sum.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) {
+        // Your code here
+        
+        map<int,int>mp;
+        mp[0]=1;
+        int sum = 0;
+        int res = 0;
+        for(int i=0;i<arr.size();i++)
+        {
+            sum += arr[i];
+            if(mp.find(sum-tar) != mp.end())
+            {
+                res += mp[sum-tar];
+            }
+            mp[sum]++;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
