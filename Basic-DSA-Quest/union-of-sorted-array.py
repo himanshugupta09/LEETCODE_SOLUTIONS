b6533e2e1e55672ef6e1ec9f421552bfd4f2cba2
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-11-10 19:56:23 +0530

    Create union-of-sorted-array.py

---
+++ Basic-DSA-Quest/union-of-sorted-array.py
@@ -0,0 +1,33 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,a,b):
+        # code here 
+        s1 = set(a)
+        s2 = set(b)
+        s3 = s1.union(s2)
+        a.clear()
+        for i in s3:
+            a.append(i)
+        a.sort()
+        return a
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        a = list(map(int, input().strip().split()))
+        b = list(map(int, input().strip().split()))
+        ob = Solution()
+        li = ob.findUnion(a, b)
+        for val in li:
+            print(val, end=' ')
+        print()
+        print("~")
+# } Driver Code Ends
