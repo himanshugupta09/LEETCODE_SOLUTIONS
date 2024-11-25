aaf6b8fa54215bca0eff1d0d0e72006491220846
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-11-25 20:11:56 +0530

    Create maximum-product-subarray.cpp

---
+++ Dynamic_Programming/maximum-product-subarray.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+        int res = INT_MIN;
+    int l=1;
+    int r = 1;
+    int n = arr.size();
+    for(int i=0;i<n;i++)
+    {
+        l *= arr[i];
+        r *= arr[n-1-i];
+        res = max(res,max(l,r));
+        if(l==0) l=1;
+        if(r==0) r = 1;
+        
+    }
+    return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
