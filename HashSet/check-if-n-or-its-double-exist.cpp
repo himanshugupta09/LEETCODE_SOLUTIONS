eb361fb24b5a425af0f5fac1e4624f5eea425c92
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-12-01 20:28:32 +0530

    Create check-if-n-or-its-double-exist.cpp

---
+++ HashSet/check-if-n-or-its-double-exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+         unordered_set<int> seen;
+        for (int num : arr) {
+            // Check if 2 * num or num / 2 exists in the set
+            if (seen.count(2 * num) || (num % 2 == 0 && seen.count(num / 2))) {
+                return true;
+            }
+            // Add the current number to the set
+            seen.insert(num);
+        }
+        // No valid pair found
+        return false;
+    }
+};
