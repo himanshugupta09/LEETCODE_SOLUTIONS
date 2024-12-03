b08f6e5160c519775b098bc574f99d8d3a72a782
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-12-03 21:09:27 +0530

    Create minimum-characters-to-be-added-at-front-to-make-string-palindrome.cpp

---
+++ Dynamic_Programming/minimum-characters-to-be-added-at-front-to-make-string-palindrome.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minChar(string& s) {
+        // Write your code here
+        string st = s;
+        reverse(st.begin(),st.end());
+        s+="$"+st;
+        int n = s.size();
+        vector<int> lps(n);
+        int len = 0;
+        lps[0]=0;
+        int i = 1;
+        while(i<n){
+            if(s[i]==s[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len==0){
+                    lps[i]=0;
+                    i++;
+                }
+                else{
+                    len = lps[len-1];
+                    
+                }
+            }
+        }
+
+        return (n/2)-lps[n-1];
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.minChar(str);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
