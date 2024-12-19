48a9d8eaae70e31a2ebdac5a4d93a931684ae35a
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-12-19 20:06:36 +0530

    Create kth-missing-positive-in-sorted-array.java

---
+++ Binary-Search/kth-missing-positive-in-sorted-array.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.kthMissing(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int low = 0;
+        int n = arr.length;
+        int high = n-1;
+        
+        while(low <= high)
+        {
+            int mid  = (low+high)/2;
+            int mis = arr[mid]-(mid+1);
+            if(mis < k)
+            {
+                low  = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return low+k;
+    }
+}
