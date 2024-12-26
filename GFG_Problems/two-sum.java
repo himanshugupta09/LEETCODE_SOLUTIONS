c653662a965a1e22b5671783c24d87e428adda05
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-12-26 18:56:18 +0530

    Create two-sum.java

---
+++ GFG_Problems/two-sum.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            // int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[0]);
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            boolean ans = new Solution().twoSum(arr, x);
+            System.out.println(ans ? "true" : "false");
+            // System.out.println("~");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+        Set<Integer>st = new HashSet<Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+            int dif  = target-arr[i];
+            if(st.contains(dif))
+            {
+                return true;
+            }
+            else
+            {
+                st.add(arr[i]);
+            }
+        }
+        return false;
+    }
+}
