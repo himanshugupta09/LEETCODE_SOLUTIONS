e124938fd42ddf92187984743dda6e56a3be0c8d
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2025-02-25 19:45:27 +0530

    Time: 2 ms (76.78%), Space: 112 MB (55.17%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    int mod = 1e9+7;
+    const int mod =  1e9+7;
     int numOfSubarrays(vector<int>& arr) {
-        int n = arr.size();
-        int res =  0;
-        int odds=0;
-        for(int i=0;i<n;i++)
+        int pre = 0;
+        int res = 0;
+        int odd = 0,eve = 1;
+        for(int i:arr)
         {
-            arr[i] %= 2;
-        }
-        vector<int>dpOdds(n),dpEve(n);
-        dpEve[n-1] = arr[n-1] == 1 ? 0 : 1;
-        dpOdds[n-1] = arr[n-1] == 1 ? 1 : 0;
-        for(int i=n-2;i>=0;i--)
-        {
-            if(arr[i] == 1)
+            pre += i;
+            if(pre%2 == 0)
             {
-                dpOdds[i] = (1+dpEve[i+1])%mod;
-                dpEve[i] = dpOdds[i+1];
+                res += odd;
+                eve++;
             }
             else
             {
-                dpEve[i] =  (1+dpEve[i+1])%mod;
-                dpOdds[i] = dpOdds[i+1];
+                res += eve;
+                odd++;
             }
-        }
-        for(auto oddse:dpOdds)
-        {
-            res += oddse;
             res %= mod;
         }
         return res;
-
+        
     }
 };
