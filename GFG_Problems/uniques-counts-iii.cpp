f30971e26e1c9b3020aa27757b307e0196f90adf
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2025-04-24 20:32:59 +0530

    Create uniques-counts-iii.cpp

---
+++ GFG_Problems/uniques-counts-iii.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int getSingle(vector<int> &arr) {
+        // code here
+        int ones = 0,two = 0;
+        for(auto i:arr)
+        {
+            ones = (ones^i)&(~two);
+            two = (two^i)&(~ones);
+        }
+        return ones;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSingle(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
