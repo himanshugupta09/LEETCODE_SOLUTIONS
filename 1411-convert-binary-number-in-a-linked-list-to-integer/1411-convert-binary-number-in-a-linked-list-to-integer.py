34bbfd2a85555276fb550ef975317d866702f4be
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2025-07-14 20:24:48 +0530

    Time: 0 ms (100%), Space: 17.9 MB (26.17%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.py
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.py
@@ -3,14 +3,11 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# We can do this in a single traversal only. 
+# Everytime we stand at a Node, we assume that it is the Last Node and we add it's value to our ans varible
+# Again, when we encounter another Node, we increment all the powers of the previous Nodes by 1, that is, multiplying our ans by 2
 
-'''
- We are standing at the Head of the Linked-List consisting of only 0 and 1. We need to return the corresponding decimal number after converting the Binary Linked-List to Decimal
-We can do this in a single traversal only. 
-Everytime we stand at a Node, we assume that it is the Last Node and we add it's value to our ans varible
-Again, when we encounter another Node, we increment all the powers of the previous Nodes by 1, that is, multiplying our ans by 2
 
-'''
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         # ans will store our final decimal number after traversing the given LinkedList
@@ -25,12 +22,4 @@ class Solution:
             ans+=(temp.val)
             temp = temp.next
         return ans
-'''
-Time Complexity:  O(N)
-Space Complexity: O(1)
 
-'''
-            
-        
-        
-        
