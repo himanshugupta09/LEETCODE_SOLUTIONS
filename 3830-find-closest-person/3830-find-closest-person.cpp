a91067a095a1160834a5fc4a44c81198ed64080f
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2025-09-04 19:31:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.11%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int d1 = abs(z-x);
+        int d2 =  abs(z-y);
+        if(d1 < d2)
+        {
+            return 1;
+        }
+        else if(d1 > d2)
+        {
+            return 2;
+        }
+        return 0;
+    }
+};
