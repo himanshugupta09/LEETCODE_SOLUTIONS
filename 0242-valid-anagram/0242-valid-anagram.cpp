2902240c6e99baf1caffd2a58a1f9e3639b8a545
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2025-10-07 20:08:35 +0530

    Time: 0 ms (100%), Space: 10 MB (11.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,4 +1,21 @@
+// Approach 1
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         int n = s.size();
+//         int m = t.size();
+//         if(n != m)
+//         {
+//             return false;
+//         }
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         return s == t;
+//     }
+// };
+
 class Solution {
+    /* Optimised Approach*/
 public:
     bool isAnagram(string s, string t) {
         int n = s.size();
@@ -7,8 +24,19 @@ public:
         {
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s == t;
+        unordered_map<char,int>mpp;
+        for(auto ch:s)
+        {
+            mpp[ch]++;
+        }
+        for(auto i:t)
+        {
+            if(mpp.find(i) == mpp.end() || mpp[i] == 0)
+            {
+                return false;
+            }
+            mpp[i]--;
+        }
+        return true;
     }
 };
