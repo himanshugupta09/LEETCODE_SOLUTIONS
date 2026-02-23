a76b3c92ff63c0915fc6d36db531a498f7e1cbb9
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-02-23 22:31:06 +0530

    Time: 255 ms (61.33%), Space: 51.9 MB (52.22%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,28 +1,7 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        # hashed = {}
-        # for nums in range(0,pow(2,k)):
-        #     binary = bin(nums)[::-1]
-        #     #print(binary)
-        #     binary = binary[:k+1]
-        #     binary =  binary[::-1].replace('b','')
-        #     #print(binary)
-        #     hashed[binary] = 0
-        #print(hashed)
-        hashed = set()
-        for i in range(0,len(s)):
-            substr = s[i:i+k]
-            #print(substr)
-            hashed.add(substr)
-        for nums in range(0,pow(2,k)+1):
-            binary = bin(nums)[::-1]
-            #print(binary)
-            binary = binary[:k+1]
-            binary =  binary[::-1].replace('b','')
-            #print(binary)
-            if len(binary) == k and binary not in hashed:
-                #print(binary)
-                return False
-        return True
-
+        seen = set()
+        for i in range(len(s) - k + 1):
+            seen.add(s[i:i+k])
+        return len(seen) == 2**k
         
