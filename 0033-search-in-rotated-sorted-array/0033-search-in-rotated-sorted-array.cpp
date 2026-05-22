09837cfc5e475df86f88590c36ff4bbbc523659d
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-05-22 17:00:37 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    int binarySearch(vector<int> &nums,int low,int high,int target)
+    int binarySearch(vector<int>&nums,int low,int high,int target)
     {
-        int l = low,r = high;
+        int l = low;
+        int r = high;
 
         while(l <= r)
         {
@@ -11,35 +12,34 @@ public:
             {
                 return mid;
             }
-            else if(nums[mid] > target)
-            {
-                r = mid-1;
-            }
-            else
+            else if(nums[mid] < target)
             {
                 l = mid+1;
             }
+            else
+            {
+                r = mid-1;
+            }
         }
         return -1;
-       
     }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-       int low = 0,high = n-1;
 
+        int low = 0;
+        int n = nums.size();
+        int high = n-1;
+        
         while(low <= high)
         {
-            int  mid = low + (high-low)/2;
-            
+            int mid = low + (high-low)/2;
             if(nums[mid] > nums[n-1])
             {
                 low = mid+1;
             }
-            else
+            else 
             {
                 high = mid-1;
             }
-
         }
         int res = binarySearch(nums,0,low-1,target);
 
@@ -48,5 +48,6 @@ public:
             return res;
         }
         return binarySearch(nums,low,n-1,target);
+        
     }
 };
