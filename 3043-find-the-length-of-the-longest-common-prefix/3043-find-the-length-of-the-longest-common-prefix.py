9b4e9743228e48a074fca5eda9efafb7acfac395
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-06-09 22:32:40 +0530

    Time: 448 ms (20.14%), Space: 34.8 MB (25.89%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,15 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        hashset = set()
-        for num in arr1:
-            
-            for i in range(len(str(num))):
-                hashset.add(str(num)[:i+1])
-        res = 0
-        for num in arr2:
-            for i in range(len(str(num))):
-                if str(num)[:i+1] in hashset:
-                    res = max(res,len(str(num)[:i])+1)
-        return res
+        arr1 = [str(i) for i in arr1]
+        arr2 = [str(i) for i in arr2]
 
-        
+        hash1 = set()
+
+        for i in range(len(arr1)):
+            for j in range(1, len(arr1[i]) + 1):
+                hash1.add(arr1[i][:j])
+
+        max_len = 0
+
+        for i in range(len(arr2)):
+            for j in range(1, len(arr2[i]) + 1):
+                prefix = arr2[i][:j]
+                if prefix in hash1:
+                    max_len = max(max_len, len(prefix))
+
+        return max_len
