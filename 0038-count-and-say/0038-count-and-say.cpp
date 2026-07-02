24964510736dfe8371da43c8f38799c8ebd1beae
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-07-02 09:34:25 +0530

    Time: 47 ms (6.62%), Space: 90 MB (5.08%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-     string makeCounter(string &st) {
-        string res;
-        int n = st.size();
-        
-        for (int i = 0; i < n; ) {
-            char currentChar = st[i];
-            int count = 0;
-            
-            // Count the number of consecutive characters
-            while (i < n && st[i] == currentChar) {
-                count++;
-                i++;
-            }
-            
-            // Append the count and the character to the result
-            res += to_string(count) + currentChar;
-        }
-        
-        return res;
-    }
     string countAndSay(int n) {
-        string cur = "1";  // Start with the base case
-
-        for (int i = 2; i <= n; i++) {
-            cur = makeCounter(cur);  // Generate the next sequence
+        if(n == 1)
+        {
+            return "1";
         }
+        if(n == 2)
+        {
+            return "11";
+        }
+        string res = "11";
+        for(int i=3;i<=n;i++)
+        {
+            string temp;
+            res  = res + '&';
+            int cnt = 1;
+            for(int j=1;j<res.length();j++)
+            {
+                if(res[j] != res[j-1])
+                {
+                    temp = temp + to_string(cnt);
+                    temp = temp + res[j-1];
+                    cnt = 1;
+                }
+                else
+                {
+                    cnt++;
+                }
+            }
+            res = temp;
 
-        return cur;
+        }
+        return res;
     }
 };
