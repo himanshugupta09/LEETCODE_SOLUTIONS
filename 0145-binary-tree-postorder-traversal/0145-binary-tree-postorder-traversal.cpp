2f456551a6a1f11d896b935e13db053529711faf
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-07-03 10:13:50 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.09%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,48 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        stack<pair<TreeNode*,int>>stk;
-        vector<int>res;
-        stk.push({root,0});
+        vector<int> res;
+        stack<TreeNode*> stk;
+        TreeNode* curr = root;
+        TreeNode* lastVis = nullptr;
 
-        while(!stk.empty())
-        {
-            auto [node,vis] = stk.top();
-            stk.pop();
-            if(!node)
-            {
-                continue;
+        while (curr || !stk.empty()) {
+
+            while (curr) {
+                stk.push(curr);
+                curr = curr->left;
             }
-            if(vis)
-            {
-                res.push_back(node->val);
+
+            TreeNode* top = stk.top();
+
+            if (top->right && lastVis != top->right) {
+                curr = top->right;
             }
-            else
-            {
-                stk.push({node,1});
-                if(node->right)
-                {
-                    stk.push({node->right,0});
-                }
-                if(node->left)
-                {
-                    stk.push({node->left,0});
-                }
+            else {
+                res.push_back(top->val);
+                lastVis = top;
+                stk.pop();
             }
         }
 
         return res;
-
     }
 };
