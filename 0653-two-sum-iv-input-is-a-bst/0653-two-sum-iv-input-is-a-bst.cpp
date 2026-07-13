8f049e73d41db70da5a00dffa62d45331869e0dc
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-07-13 08:54:18 +0530

    Time: 4 ms (42.1%), Space: 38.8 MB (33.03%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,41 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool hasSum = false;
+    unordered_set<int> seen;
 
-    set<int>nums;
+    bool dfs(TreeNode* root, int k) {
+        if (!root) return false;
 
-    void helper(TreeNode* root,int k)
-    {
-        if(!root)
-        {
-            return;
-        }
-        if(nums.find(k-root->val) != nums.end())
-        {
-            hasSum = true;
-            return;
-        }
-        else
-        {
-            nums.insert(root->val);
-            helper(root->left,k);
-            helper(root->right,k);
-        }
-        return;
+        if (seen.count(k - root->val))
+            return true;
+
+        seen.insert(root->val);
+
+        return dfs(root->left, k) || dfs(root->right, k);
     }
+
     bool findTarget(TreeNode* root, int k) {
-        helper(root,k);
-        return hasSum;
+        return dfs(root, k);
     }
 };
