7530defafc50af1339c2d98f56399c9ca2d8b7ac
himanshugupta09 <hg7984152@gmail.com>
2026-07-28 22:02:58 +0530

    BackTrack

---
+++ String/all-subsequnce-of-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def solve(self,s,i,res,path):
+        if i == len(s):
+            res.append(path)
+            return
+        path += s[i]
+        self.solve(s,i+1,res,path)
+        path = path[:-1]
+        self.solve(s,i+1,res,path)
+        return
+    def powerSet(self, s):
+		# Code here
+		res = []
+		temp = ''
+		self.solve(s,0,res,temp)
+		res.sort()
+		return res
+Solution().powerSet("abc")
+		
