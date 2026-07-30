7f4333c9d9d1242cae3f1ecad5c9019eb888a115
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-07-30 22:22:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (88.17%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,14 +1,17 @@
 class Solution:
+    def solve(self,idx,nums,res,arr):
+        if idx == len(nums):
+            res.append(arr[:])
+            return
+        arr.append(nums[idx])
+        self.solve(idx+1,nums,res,arr)
+        arr.pop()
+        while idx + 1 < len(nums) and nums[idx] == nums[idx + 1]:
+            idx += 1
+        self.solve(idx+1,nums,res,arr)
+
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         res = []
-        def backtrack(start,path):
-            res.append(path[:])
-            for i in range(start,len(nums)):
-                if i > start and nums[i] == nums[i-1]:
-                    continue
-                path.append(nums[i])
-                backtrack(i+1,path)
-                path.pop()
-        backtrack(0,[])
+        nums.sort()
+        self.solve(0,nums,res,[])
         return res
