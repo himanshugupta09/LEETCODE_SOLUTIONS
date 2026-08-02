274e759c8f029412571203c53ddb7cbae408c8de
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-08-02 14:45:18 +0530

    Time: 3 ms (55.5%), Space: 20.1 MB (11.56%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,17 @@
+from functools import cache
 class Solution:
-    def solve(self,nums,i,j):
-        if i == j:
-            return nums[i]
-        choice_a = nums[i]  - self.solve(nums,i+1,j)
-        choice_b = nums[j] - self.solve(nums,i,j-1)
-        return max(choice_a,choice_b)
 
     def predictTheWinner(self, nums: List[int]) -> bool:
         i = 0
         j = len(nums)-1
-        
-        score_d = self.solve(nums,i,j)
+        @cache
+        def solve(i,j):
+            if i == j:
+                return nums[i]
+            choice_a = nums[i]  - solve(i+1,j)
+            choice_b = nums[j] - solve(i,j-1)
+            return max(choice_a,choice_b)
+        score_d = solve(i,j)
         return score_d >= 0
         
         
