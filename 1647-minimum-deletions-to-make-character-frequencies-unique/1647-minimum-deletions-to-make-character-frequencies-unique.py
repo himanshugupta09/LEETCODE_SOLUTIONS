b60068e06ee9ca44605d82325281f234581a4316
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-08-07 22:15:28 +0530

    Time: 67 ms (76.68%), Space: 20.2 MB (78.61%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,30 +1,13 @@
+from collections import Counter
 class Solution:
     def minDeletions(self, s: str) -> int:
-        freq = set()
-        ls = [i for i in s]
-        ls.sort()
-        last = ls[0]
-        cnt = 1
+        freq = Counter(s)
+        seen_freq = set()
         dels = 0
-        
-        ls.append('$')
-        for i in range(1,len(ls)):
-            if ls[i] != last:
-                if cnt not in freq:
-                    freq.add(cnt)
-                    cnt = 1
-                    last  = ls[i]
-                else:
-                    while cnt > 0  and cnt in freq:
-                        cnt -= 1
-                        dels += 1
-                    if cnt == 0:
-                        cnt = 1
-                        last = ls[i]
-                    else:
-                        freq.add(cnt)
-                        cnt = 1
-                        last = ls[i]
-            else:
-                cnt += 1
+        for val in freq.values():
+            while val > 0 and val in seen_freq:
+                val -= 1
+                dels += 1
+            if val  > 0:
+                seen_freq.add(val)
         return dels
