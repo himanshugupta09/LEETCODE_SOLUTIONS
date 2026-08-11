bb9d432f99ea20d068dd32c869f45dad9b060c67
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2026-08-11 10:02:32 +0530

    Time: 687 ms (45.51%), Space: 20.1 MB (55.09%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,16 +1,18 @@
-from functools import cache
 class Solution:
-    def winnerSquareGame(self, n: int) -> bool:       
-        @cache
-        def dp(n):
-            if n == 0:
-                return False
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+
+        # dp[0] = False
+        # No move possible when n = 0
+
+        for i in range(1, n + 1):
             x = 1
-            while x*x <= n:
-                took = dp(n-x*x)
-                if took == False:
-                    return True
+
+            while x * x <= i:
+                if dp[i - x * x] == False:
+                    dp[i] = True
+                    break
+
                 x += 1
-            return False
-        return dp(n)
-                
+
+        return dp[n]
